3431a82627c3097bc37d5a3dcf3e6844e75502f0
Nadhira <nadhirailmii@gmail.com>
2024-12-27 11:59:39 +0800

    pls done

--- src/components/InveSa.vue
+++ src/components/InveSa.vue
@@ -1,40 +1,136 @@
 <template>
-    <div id="inves" class="flex items-center justify-center py-10 px-8 md:px-14 text-center mx-auto font-semibold h-fit bg-gradient-to-b from-[#08203D] via-[#2B4B69] to-[#08203D]">
-        <div>
-            <h1 class="text-white text-2xl mb-10">Yuk, Kerjasama Investasi Modal Usaha !</h1>
+    <div id="inves" class="items-center justify-center py-10 px-8 md:px-24 text-center mx-auto font-semibold h-fit bg-gradient-to-b from-[#08203D] via-[#2B4B69] to-[#08203D]">
 
-            <div class="md:flex justify-center my-1 text-base md:text-xl">
-                <div class="text-sm md:text-lg px-10 py-1 my-2 md:my-0 bg-white mx-1 rounded-sm">
-                    <h1>Kafe</h1>
+        <h1 class="mb-10 text-lg md:text-2xl text-white">Yuk, Kerjasama Investasi Modal Usaha !</h1>
+
+        <div class="md:flex justify-around w-full hidden">
+
+            <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                <div class="py-2 bg-primary2">
                 </div>
-                <div class="text-sm md:text-lg px-10 py-1 my-2 md:my-0 bg-white mx-1 rounded-sm">
-                    <h1>Resto</h1>
-                </div>
-                <div class="text-sm md:text-lg px-10 py-1 my-2 md:my-0 bg-white mx-1 rounded-sm">
-                    <h1>Home Stay</h1>
-                </div>
+                <h1 class="text-8xl font-bold">01</h1>
+                <p class="text-left text-xl">Kafe</p>
+                <p class="text-left font-light">Kerjasama Investasi Modal Usaha Untuk Pengelolaan kafe</p>
             </div>
 
-            <div class="md:flex justify-center md:my-2 text-base md:text-xl">
-                <div class="text-sm md:text-lg px-10 py-1 my-2 md:my-0 bg-white mx-1 rounded-sm">
-                    <h1>NYAMANNA Home Industri</h1>
+            <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                <div class="py-2 bg-primary2">
                 </div>
-                <div class="text-sm md:text-lg px-10 py-1 my-2 md:my-0 bg-white mx-1 rounded-sm">
-                    <h1>Working Space</h1>
-                </div>
-                <div class="text-sm md:text-lg px-10 py-1 my-2 md:my-0 bg-white mx-1 rounded-sm">
-                    <h1>Ambisigo Mart</h1>
-                </div>
+                <h1 class="text-8xl font-bold">02</h1>
+                <p class="text-left text-xl">Resto</p>
+                <p class="text-left font-light">Kerjasama Investasi Modal Usaha Untuk Pengelolaan resto</p>
             </div>
 
-            <div class="flex justify-center my-2 text-base md:text-xl">
-                <div class="text-sm md:text-lg px-10 py-1 bg-white mx-1 rounded-sm">
-                    <h1>Rumah tahfidz</h1>
+            <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                <div class="py-2 bg-primary2">
                 </div>
-                <div class="text-sm md:text-lg px-10 py-1 bg-white mx-1 rounded-sm">
-                    <h1>Perumahan Cluster Villa Nyamanna</h1>
-                </div>
+                <h1 class="text-8xl font-bold">03</h1>
+                <p class="text-left text-xl">Homestay</p>
+                <p class="text-left font-light">Kerjasama Investasi Modal Usaha Untuk Pengelolaan KARESOH NYAMANNA Home Industri</p>
             </div>
+
+            <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                <div class="py-2 bg-primary2">
+                </div>
+                <h1 class="text-8xl font-bold">04</h1>
+                <p class="text-left text-xl">Working Space</p>
+                <p class="text-left font-light">Kerjasama Investasi Modal Usaha Untuk Pengelolaan working space</p>
+            </div>
+
+            <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                <div class="py-2 bg-primary2">
+                </div>
+                <h1 class="text-8xl font-bold">05</h1>
+                <p class="text-left text-xl">Ambisigo Mart</p>
+                <p class="text-left font-light">Menjual Kebutuhan harian di Perumahan Cluster Villa Nyamanna</p>
+            </div>
+
+            <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                <div class="py-2 bg-primary2">
+                </div>
+                <h1 class="text-8xl font-bold">06</h1>
+                <p class="text-left text-xl">Rumah Tahfidz</p>
+                <p class="text-left font-light">Rumah tahfidz di perumahan Bumi Borongloe Permai</p>
+            </div>
+
         </div>
+
+
+
+
+
+        <div class="md:hidden block">
+
+                        
+
+            <div id="default-carousel" class="relative w-full" data-carousel="slide">
+                <!-- Carousel wrapper -->
+                <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                    <!-- Item 1 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                            <div class="py-2 bg-primary2">
+                            </div>
+                            <h1 class="text-8xl font-bold">01</h1>
+                            <p class="text-left text-xl">Kafe</p>
+                            <p class="text-left font-light">Kerjasama Investasi Modal Usaha Untuk Pengelolaan kafe</p>
+                        </div>
+                    </div>
+                    <!-- Item 2 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                            <div class="py-2 bg-primary2">
+                            </div>
+                            <h1 class="text-8xl font-bold">02</h1>
+                            <p class="text-left text-xl">Resto</p>
+                            <p class="text-left font-light">Kerjasama Investasi Modal Usaha Untuk Pengelolaan resto</p>
+                        </div>
+                    </div>
+                    <!-- Item 3 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                            <div class="py-2 bg-primary2">
+                            </div>
+                            <h1 class="text-8xl font-bold">03</h1>
+                            <p class="text-left text-xl">Homestay</p>
+                            <p class="text-left font-light">Kerjasama Investasi Modal Usaha Untuk Pengelolaan KARESOH NYAMANNA Home Industri</p>
+                        </div>
+                    </div>
+                    <!-- Item 4 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                            <div class="py-2 bg-primary2">
+                            </div>
+                            <h1 class="text-8xl font-bold">04</h1>
+                            <p class="text-left text-xl">Working Space</p>
+                            <p class="text-left font-light">Kerjasama Investasi Modal Usaha Untuk Pengelolaan working space</p>
+                        </div>
+                    </div>
+                    <!-- Item 5 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                            <div class="py-2 bg-primary2">
+                            </div>
+                            <h1 class="text-8xl font-bold">05</h1>
+                            <p class="text-left text-xl">Ambisigo Mart</p>
+                            <p class="text-left font-light">Menjual Kebutuhan harian di Perumahan Cluster Villa Nyamanna</p>
+                        </div>
+                    </div>
+                    <!-- Item 6 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <div class="text-white w-36 md:mx-0 mx-auto md:my-2">
+                            <div class="py-2 bg-primary2">
+                            </div>
+                            <h1 class="text-8xl font-bold">06</h1>
+                            <p class="text-left text-xl">Rumah Tahfidz</p>
+                            <p class="text-left font-light">Rumah tahfidz di perumahan Bumi Borongloe Permai</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
     </div>
 </template>
--- src/components/TesTi.vue
+++ src/components/TesTi.vue
@@ -1,76 +1,76 @@
 <template>
 
     <div class="md:pt-20 md:pb-10">
-        
-        <div id="default-carousel" class="relative w-full" data-carousel="slide">
+
+    <div class="w-full bg-white rounded-md my-2 h-[450px]">
+
+        <div id="carousel" class="relative w-full " data-carousel="static"> <!-- Updated: disable auto-slide -->
             <!-- Carousel wrapper -->
-            <div class="relative overflow-hidden rounded-lg h-[450px] md:h-[400px] py-auto justify-center align-middle">
+            <div class="relative h-[450px] overflow-hidden rounded-t-md">
                 <!-- Item 1 -->
                 <div class="hidden duration-700 ease-in-out bg-white px-16 md:px-36" data-carousel-item>
-                        <div class="bg-white drop-shadow-2xl mt-16 p-5 rounded-md ">
-                            <img src="/public/assets/“.png" alt="" class="md:w-10 md:h-8 w-8">
+                    <div class="bg-white drop-shadow-2xl mt-16 p-5 rounded-md ">
+                        <img src="/public/assets/“.png" alt="" class="md:w-10 md:h-8 w-8">
 
-                            <div class="flex justify-between">
-                                <img src="/public/assets/d 1.png" alt="" class="mx-auto border-[10px] border-white h-28 w-28 mt-[-100px] rounded-full">
-                            </div>
-
-                            <p class="text-center w-[70%] mx-auto my-2 md:my-8 md:text-base text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cupiditate veritatis omnis quis voluptates accusantium, dolorum voluptatum incidunt nulla quisquam exercitationem quo sapiente. Est tempora eos aperiam, fugiat mollitia minima.</p>
-
-                            <p class="text-center font-bold md:text-base text-xs">Celebes Digital</p>
-
-                            <div class="flex justify-between">
-                                <div></div>
-                                <img src="/public/assets/”.png" alt="" class="justify-end md:w-10 md:h-8 w-8">
-                            </div>
+                        <div class="flex justify-between">
+                            <img src="/public/assets/d 1.png" alt="" class="mx-auto border-[10px] border-white h-28 w-28 mt-[-100px] rounded-full">
                         </div>
+
+                        <p class="text-center w-[70%] mx-auto my-2 md:my-8 md:text-base text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cupiditate veritatis omnis quis voluptates accusantium, dolorum voluptatum incidunt nulla quisquam exercitationem quo sapiente. Est tempora eos aperiam, fugiat mollitia minima.</p>
+
+                        <p class="text-center font-bold md:text-base text-xs">Celebes Digital</p>
+
+                        <div class="flex justify-between">
+                            <div></div>
+                            <img src="/public/assets/”.png" alt="" class="justify-end md:w-10 md:h-8 w-8">
+                        </div>
+                    </div>
                 </div>
                 <!-- Item 2 -->
                 <div class="hidden duration-700 ease-in-out bg-white px-16 md:px-36" data-carousel-item>
                     <div class="bg-white drop-shadow-2xl mt-16 p-5 rounded-md ">
-                            <img src="/public/assets/“.png" alt="" class="md:w-10 md:h-8 w-8">
+                        <img src="/public/assets/“.png" alt="" class="md:w-10 md:h-8 w-8">
 
-                            <div class="flex justify-between">
-                                <img src="/public/assets/d 1.png" alt="" class="mx-auto border-[10px] border-white h-28 w-28 mt-[-100px] rounded-full">
-                            </div>
+                        <div class="flex justify-between">
+                            <img src="/public/assets/d 1.png" alt="" class="mx-auto border-[10px] border-white h-28 w-28 mt-[-100px] rounded-full">
+                        </div>
 
-                            <p class="text-center w-[70%] mx-auto my-2 md:my-8 md:text-base text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cupiditate veritatis omnis quis voluptates accusantium, dolorum voluptatum incidunt nulla quisquam exercitationem quo sapiente. Est tempora eos aperiam, fugiat mollitia minima.</p>
+                        <p class="text-center w-[70%] mx-auto my-2 md:my-8 md:text-base text-xs">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, accusantium architecto rerum praesentium dolores accusamus laudantium debitis cumque numquam possimus.</p>
 
-                            <p class="text-center font-bold md:text-base text-xs">Celebes Digital</p>
+                        <p class="text-center font-bold md:text-base text-xs">Celebes Digital</p>
 
-                            <div class="flex justify-between">
-                                <div></div>
-                                <img src="/public/assets/”.png" alt="" class="justify-end md:w-10 md:h-8 w-8">
-                            </div>
+                        <div class="flex justify-between">
+                            <div></div>
+                            <img src="/public/assets/”.png" alt="" class="justify-end md:w-10 md:h-8 w-8">
+                        </div>
                     </div>
                 </div>
-                <!-- 33 -->
-                <div class="hidden duration-700 ease-in-out bg-white px-16 md:px-36" data-carousel-item>
+                <!-- Item 3 -->
+                <div class="hidden my-auto justify-center align-middle duration-700 ease-in-out bg-white px-16 md:px-36" data-carousel-item>
                     <div class="bg-white drop-shadow-2xl mt-16 p-5 rounded-md ">
-                            <img src="/public/assets/“.png" alt="" class="md:w-10 md:h-8 w-8">
+                        <img src="/public/assets/“.png" alt="" class="md:w-10 md:h-8 w-8">
 
-                            <div class="flex justify-between">
-                                <img src="/public/assets/d 1.png" alt="" class="mx-auto border-[10px] border-white h-28 w-28 mt-[-100px] rounded-full">
-                            </div>
+                        <div class="flex justify-between">
+                            <img src="/public/assets/d 1.png" alt="" class="mx-auto border-[10px] border-white h-28 w-28 mt-[-100px] rounded-full">
+                        </div>
 
-                            <p class="text-center w-[70%] mx-auto my-2 md:my-8 md:text-base text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cupiditate veritatis omnis quis voluptates accusantium, dolorum voluptatum incidunt nulla quisquam exercitationem quo sapiente. Est tempora eos aperiam, fugiat mollitia minima.</p>
+                        <p class="text-center w-[70%] mx-auto my-2 md:my-8 md:text-base text-xs">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio provident inventore quasi earum obcaecati ipsam eaque amet doloremque eveniet architecto!</p>
 
-                            <p class="text-center font-bold md:text-base text-xs">Celebes Digital</p>
+                        <p class="text-center font-bold md:text-base text-xs">Celebes Digital</p>
 
-                            <div class="flex justify-between">
-                                <div></div>
-                                <img src="/public/assets/”.png" alt="" class="justify-end md:w-10 md:h-8 w-8">
-                            </div>
+                        <div class="flex justify-between">
+                            <div></div>
+                            <img src="/public/assets/”.png" alt="" class="justify-end md:w-10 md:h-8 w-8">
+                        </div>
                     </div>
                 </div>
+                <!-- Additional items as needed -->
             </div>
-            <!-- Slider indicators -->
-            <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-            </div>
+
+
             <!-- Slider controls -->
             <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
                     <svg class="w-4 h-4 text-black rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
                     </svg>
@@ -78,7 +78,7 @@
                 </span>
             </button>
             <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
                     <svg class="w-4 h-4 text-black rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
                     </svg>
@@ -87,9 +87,9 @@
             </button>
         </div>
 
-
-
     </div>
+
+</div>
     
 
 </template>
